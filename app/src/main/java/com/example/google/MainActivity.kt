7dd076b88e12a8181a6685a125ab4c3619b9e2fe
unknown <sadim7m@gmail,com>
2022-12-21 14:39:49 +0000

    initial commit

---
+++ app/src/main/java/com/example/google/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.google
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
